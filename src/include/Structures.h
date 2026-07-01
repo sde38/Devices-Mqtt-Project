04c406cceeb3202993dd5fd0c88ad810690a22ef
Dessolas <sdessolas@gmail.com>
2026-07-01 08:37:48 +0200

    Ajout concept (C++20)

--- src/include/Structures.h
+++ src/include/Structures.h
@@ -27,27 +27,24 @@ struct MsgAjout {
     int temperature;
     int charge;
 };
-
 // to_json/from_json générés automatiquement par nlohmann (macro intrusive)
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(MsgAjout, nom, type, temperature, charge)
-MQTT_JSON_SERIALIZER(MsgAjout)
+
 
 struct MsgSuppression {
     int id;
 };
-
 // to_json/from_json générés automatiquement par nlohmann (macro intrusive)
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(MsgSuppression, id)
-MQTT_JSON_SERIALIZER(MsgSuppression)
+
 
 struct MsgDataInt {
     int id;
     int data;
 };
-
 // to_json/from_json générés automatiquement par nlohmann (macro intrusive)
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(MsgDataInt, id, data)
-MQTT_JSON_SERIALIZER(MsgDataInt)
+
 
 
 #endif // STRUCTURES_H
